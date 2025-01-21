c9f34b73a34a930adfeb6572ec2b9b274e17da2c
JuniorzGTX <96076596+JuniorzGTX@users.noreply.github.com>
2025-01-21 07:03:20 +0000

    Submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+age = int(input("What is your age?"))
+
+#Print
+
+print(f"Hello {name}, you are {age} years old.")
+print(f"Next year, you will be {age + 1} years old")
+
+#gyatt
